dfcafb9d55da3ad357f53724ece34edc3603c53d
kajsa99 <kajsa.palmen@gmail.com>
2026-02-25 14:15:40 +0100

    css to cypress components tests

--- frontend/cypress/support/component.ts
+++ frontend/cypress/support/component.ts
@@ -16,6 +16,9 @@
 // Import commands.js using ES2015 syntax:
 import "./commands";
 
+// Import styles
+import "../../src/index.css";
+
 import { mount } from "cypress/react";
 
 // Augment the Cypress namespace to include type definitions for
